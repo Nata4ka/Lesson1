bd7b7609bf427b4ae0c33fe7a008e55f5faf2e71
Natalia Novohatskaja <natalinovohatskaja@gmail.com>
2023-02-16 21:07:47 +0300

    Added min() and average() methods in SalesManager class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[]args){
-        int[] sales = new int[] {4,2,100,400};
+        int[] sales = new int[] {4,2,100,400,50};
         SalesManager salesManager = new SalesManager(sales);
         int max = salesManager.max();
+        int min = salesManager.min();
+        int average = salesManager.average();
         System.out.println(max);
+        System.out.println(min);
+        System.out.println(average);
 
     }
 }
