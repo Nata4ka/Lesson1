855c944a69b2f5227aab4446d173f3f5adb832ea
Natalia Novohatskaja <natalinovohatskaja@gmail.com>
2023-02-16 18:32:26 +0300

    changed int to long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[]args){
-        int[] sales = new int[] {4,2,100,400};
+        long[] sales = new long[] {4,2,100,400};
         SalesManager salesManager = new SalesManager(sales);
-        int max = salesManager.max();
+        long max = salesManager.max();
         System.out.println(max);
 
     }
